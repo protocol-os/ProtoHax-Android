1bf7ced4612340ac2df7ca15705f5cd2cebd2839
protocol-os <info.strikenoob@gmail.com>
2024-05-10 17:57:03 +0600

    Update AppService.kt

--- app/src/main/java/dev/sora/protohax/AppService.kt
+++ app/src/main/java/dev/sora/protohax/AppService.kt
@@ -1,58 +1,60 @@
-package dev.sora.protohax;
+package dev.sora.protohax
 
-import android.app.NotificationManager;
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.view.WindowManager;
-import androidx.core.app.NotificationCompat;
-import com.github.megatronking.netbare.NetBare;
-import com.github.megatronking.netbare.NetBareService;
-import dev.sora.protohax.ContextUtils.toast;
-import dev.sora.protohax.forwarder.R;
-import kotlin.random.Random;
+import android.app.*
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.view.WindowManager
+import androidx.core.app.NotificationCompat
+import com.github.megatronking.netbare.NetBare
+import com.github.megatronking.netbare.NetBareService
+import dev.sora.protohax.ContextUtils.toast
+import dev.sora.protohax.forwarder.R
+import kotlin.random.Random
 
-class AppService extends NetBareService {
 
-    private WindowManager windowManager;
+class AppService : NetBareService() {
 
-    @Override
-    public void onCreate() {
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Service.NOTIFICATION_SERVICE);
+    private lateinit var windowManager: WindowManager
+
+    override fun onCreate() {
+        val notificationManager = getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
-            notificationManager.createNotificationChannel(new NotificationChannel(CHANNEL_ID, getString(R.string.app_name), NotificationManager.IMPORTANCE_LOW));
+            notificationManager.createNotificationChannel(NotificationChannel(CHANNEL_ID, getString(R.string.app_name), NotificationManager.IMPORTANCE_LOW))
         }
 
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
     }
 
-    @Override
-    public int notificationId() {
-        return Random.Default.nextInt();
-    }
+    override fun notificationId() = Random.Default.nextInt()
 
-    @Override
-    public Notification createNotification() {
-        int flag = PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+    override fun createNotification(): Notification {
+        val flag = PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        intent.setAction(Intent.ACTION_MAIN);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, flag);
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        intent.action = Intent.ACTION_MAIN
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+//        val stopIntent = Intent(ACTION_STOP)
+//        stopIntent.setPackage(packageName)
+//        val pendingIntent1 = PendingIntent.getActivity(this, 1, stopIntent, flag)
 
-        builder
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        
+            builder
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText(getString(R.string.proxy_notification, getString(R.string.app_name), NetBare.get().config.allowedApplications.firstOrNull() ?: "unknown"))
                 .setSmallIcon(R.drawable.notification_icon)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
                 .setOngoing(true)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+//                .addAction(R.drawable.notification_icon, getString(R.string.stop_proxy_notify), pendingIntent1)
+        }
 
-        return builder.build();
+        return builder.build()
     }
 
-    public static final String CHANNEL_ID = "dev.sora.protohax.NOTIFICATION_CHANNEL_ID";
+    companion object {
+        const val CHANNEL_ID = "dev.sora.protohax.NOTIFICATION_CHANNEL_ID"
+    }
 }
