15661a4bcce607b58c39609ccdfd7229a5177f91
protocol-os <info.strikenoob@gmail.com>
2024-05-10 20:40:16 +0600

    Update CreditsActivity.java

--- app/src/main/java/dev/sora/protohax/CreditsActivity.java
+++ app/src/main/java/dev/sora/protohax/CreditsActivity.java
@@ -1,13 +1,12 @@
-package dev.sora.protohax
+package dev.sora.protohax;
 
-import android.app.*
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class CreditsActivity extends AppCompatActivity {
+public class CreditsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +21,9 @@ public class CreditsActivity extends AppCompatActivity {
         // Set the title and text content
         TextView creditsTextView = findViewById(R.id.creditsTextView);
         creditsTextView.setText("Made by Strike");
+
+        // Any other UI setup or logic can go here
     }
+
+    // Any additional methods or logic can be added below
 }
