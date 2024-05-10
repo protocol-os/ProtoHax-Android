ef9e12f3ef301456c29748fea20ec8aec8f4ad7c
protocol-os <info.strikenoob@gmail.com>
2024-05-10 18:02:09 +0600

    Update AppService.kt

--- app/src/main/java/dev/sora/protohax/AppService.kt
+++ app/src/main/java/dev/sora/protohax/AppService.kt
@@ -11,7 +11,6 @@ import dev.sora.protohax.ContextUtils.toast
 import dev.sora.protohax.forwarder.R
 import kotlin.random.Random
 
-
 class AppService : NetBareService() {
 
     private lateinit var windowManager: WindowManager
@@ -19,7 +18,13 @@ class AppService : NetBareService() {
     override fun onCreate() {
         val notificationManager = getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
-            notificationManager.createNotificationChannel(NotificationChannel(CHANNEL_ID, getString(R.string.app_name), NotificationManager.IMPORTANCE_LOW))
+            notificationManager.createNotificationChannel(
+                NotificationChannel(
+                    CHANNEL_ID,
+                    getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_LOW
+                )
+            )
         }
 
         windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
@@ -35,26 +40,27 @@ class AppService : NetBareService() {
         intent.action = Intent.ACTION_MAIN
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
 
-//        val stopIntent = Intent(ACTION_STOP)
-//        stopIntent.setPackage(packageName)
-//        val pendingIntent1 = PendingIntent.getActivity(this, 1, stopIntent, flag)
+        val stopIntent = Intent(ACTION_STOP)
+        stopIntent.setPackage(packageName)
+        val pendingIntent1 = PendingIntent.getActivity(this, 1, stopIntent, flag)
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-        
-            builder
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(getString(R.string.proxy_notification, getString(R.string.app_name), NetBare.get().config.allowedApplications.firstOrNull() ?: "unknown"))
-                .setSmallIcon(R.drawable.notification_icon)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
-                .setOngoing(true)
-                .setContentIntent(pendingIntent)
-//                .addAction(R.drawable.notification_icon, getString(R.string.stop_proxy_notify), pendingIntent1)
-        }
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(
+                getString(
+                    R.string.proxy_notification,
+                    getString(R.string.app_name),
+                    NetBare.get().config.allowedApplications.firstOrNull() ?: "unknown"
+                )
+            )
+            .setSmallIcon(R.drawable.notification_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setOngoing(true)
+            .setContentIntent(pendingIntent)
+            .addAction(R.drawable.notification_icon, getString(R.string.stop_proxy_notify), pendingIntent1)
 
         return builder.build()
     }
 
-    companion object {
-        const val CHANNEL_ID = "dev.sora.protohax.NOTIFICATION_CHANNEL_ID"
-    }
+    const val CHANNEL_ID = "dev.sora.protohax.NOTIFICATION_CHANNEL_ID"
 }
