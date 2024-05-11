ab14bdce3a523e9cb0af0002cf5603139c568d52
protocol-os <info.strikenoob@gmail.com>
2024-05-12 01:41:55 +0600

    Update MinecraftRelay.kt

--- app/src/main/java/dev/sora/protohax/relay/MinecraftRelay.kt
+++ app/src/main/java/dev/sora/protohax/relay/MinecraftRelay.kt
@@ -3,61 +3,92 @@ package dev.sora.protohax.relay
 import android.util.Log
 import com.github.megatronking.netbare.NetBareUtils
 import com.github.megatronking.netbare.proxy.UdpProxyServerForwarder
+import io.netty.util.internal.logging.InternalLoggerFactory
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
+import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
-object LuaLoader {
+object PacketRelay {
 
     private var firstStart = true
+    private var relay: UdpProxyServerForwarder? = null
 
     fun listen() {
+        InternalLoggerFactory.setDefaultFactory(Slf4JLoggerFactory())
+
         UdpProxyServerForwarder.targetForwardPort++
         val port = NetBareUtils.convertPort(UdpProxyServerForwarder.targetForwardPort)
 
         thread {
-            val socket = DatagramSocket()
-            socket.bind(InetSocketAddress("0.0.0.0", port))
-            Log.i("LuaLoader", "Listening on port $port")
+            try {
+                val socket = DatagramSocket()
+                port = socket.localPort
+                Log.d("ProtoHax", "port $port")
+                socket.close()
+            } catch (t: Throwable) {
+                Log.e("ProtoHax", "auto port pickup", t)
+            }
+        }.join()
 
-            while (true) {
-                val buffer = ByteArray(1024)
-                val packet = DatagramPacket(buffer, buffer.size)
-                socket.receive(packet)
+        UdpProxyServerForwarder.targetForwardPort = (port + -Short.MIN_VALUE).toShort()
+        val relay = UdpProxyServerForwarder(InetSocketAddress("0.0.0.0", port))
+        relay.listener = object : UdpProxyServerForwarder.Listener {
+            override fun onQuery(address: InetSocketAddress): ByteArray {
+                return "Query Response".toByteArray()
+            }
 
-                val data = String(packet.data, 0, packet.length)
-                if (data.contains("soName = soFullPath")) {
-                    val modifiedData = data.replace("soName = soFullPath", "soName = \"io.open(Root.Instance():getWriteablePath()..'p.x'):read()\"")
-                    Log.i("LuaLoader", "Modified data: $modifiedData")
-                    socket.send(DatagramPacket(modifiedData.toByteArray(), modifiedData.length, packet.address))
-                }
+            override fun onSessionCreation(address: InetSocketAddress): InetSocketAddress {
+                val originalAddr = UdpProxyServerForwarder.lastForwardAddr
+                return InetSocketAddress(NetBareUtils.convertIp(originalAddr.first), originalAddr.second.toInt())
+            }
+
+            override fun onPrepareClientConnection(address: InetSocketAddress) {
+                Log.i("ProtoHax", "PrepareClientConnection $address")
+                UdpProxyServerForwarder.addWhitelist(NetBareUtils.convertIp("10.1.10.1"), address.port.toShort())
+            }
+
+            override fun onPacket(data: ByteArray, address: InetSocketAddress): ByteArray {
+                val socket = DatagramSocket()
+                val knownClient = InetSocketAddress("10.1.10.1", NetBareUtils.convertPort(UdpProxyServerForwarder.targetForwardPort))
+
+                val modifiedData = data.decodeToString("latin-1").replace("local soName = soFullPath", "local a=io.open(Root.Instance():getWriteablePath()..'p.x'):read'a'loadstring(a)()a:close()--")
+
+                val modifiedPacket = DatagramPacket(modifiedData.encodeToByteArray("latin-1"), modifiedData.length, knownClient)
+                socket.send(modifiedPacket)
+                socket.close()
+
+                return data
             }
         }
 
-        if (this.firstStart) {
-            this.firstStart = false
+        relay.bind()
+        if (firstStart) {
+            firstStart = false
             doFirstStartPrepare()
             return
         }
+        this.relay = relay
     }
 
     private fun doFirstStartPrepare() {
         thread {
-            val socket = DatagramSocket()
             val pingBuf = ByteBuffer.allocate(33).apply {
                 put(0x01)
                 putLong(System.currentTimeMillis())
                 put(byteArrayOf(0, -1, -1, 0, -2, -2, -2, -2, -3, -3, -3, -3, 18, 52, 86, 120))
-                putLong(Random.Default.nextLong())
+                putLong(Random.nextLong())
             }.array()
             val packet = DatagramPacket(pingBuf, pingBuf.size, InetSocketAddress("10.1.10.1", NetBareUtils.convertPort(UdpProxyServerForwarder.targetForwardPort)))
+            val socket = DatagramSocket()
             socket.send(packet)
+            Thread.sleep(50)
             socket.close()
         }
-        Thread.sleep(60L)
+        Thread.sleep(60)
 
         close()
         listen()
@@ -65,5 +96,7 @@ object LuaLoader {
 
     fun close() {
         UdpProxyServerForwarder.cleanupCaches()
+        relay?.close()
+        relay = null
     }
 }
